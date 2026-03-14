90f453c66dcfa7df4b92c5cb4e91acbb92a44408
dashaw92 <dashaw92@gmail.com>
2026-03-14 09:12:09 -0400

    Test extractors

--- backend/bin/backend.dart
+++ backend/bin/backend.dart
@@ -1,7 +1,7 @@
 import 'package:grpc/grpc.dart';
 import 'package:pantry_protocol/protocol.dart' hide Extractor;
 import 'package:protobuf/well_known_types/google/protobuf/empty.pb.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 
 import 'crud.dart';
 import 'extractors.dart';
@@ -75,7 +75,7 @@ Future<GrpcError?> logging(ServiceCall call, ServiceMethod method) async {
 
 void main(List<String> args) async {
   final extractors = await initExtractors();
-  test(extractors.first);
+  // test(extractors.first);
   final server = Server.create(
     services: [PantryService(extractors: extractors)],
     codecRegistry: CodecRegistry(codecs: const [GzipCodec(), IdentityCodec()]),
@@ -85,11 +85,14 @@ void main(List<String> args) async {
   print('Server started on port ${server.port}');
 }
 
-void test(Extractor extractor) async {
-  final url = Uri.parse(
-    "https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/",
-  );
-  final body = await http.read(url);
-  final output = extractor.extract(body);
-  print(output);
-}
+// void test(Extractor extractor) async {
+//   final url = Uri.parse(
+//     "https://sallysbakingaddiction.com/chewy-chocolate-chip-cookies/",
+//   );
+//   final url = Uri.parse(
+//     "https://www.allrecipes.com/pretzel-millionaire-bars-recipe-11838012",
+//   );
+//   final body = await http.read(url);
+//   final output = extractor.extract(body);
+//   print(output);
+// }
