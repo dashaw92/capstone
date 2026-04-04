b29f157d0aac6fce42e6c256d74853a1bcbd2619
dashaw92 <dashaw92@gmail.com>
2026-04-04 19:09:51 -0400

    protocol documentation v2

--- protocol/clean.sh
+++ protocol/clean.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# For whatever reason, protoc refused to overwrite existing files. Bypass this requirement by deleting the base directory.
 rm -rf lib/src/generated
+# ... and then it's also weirdly incapable of *creating* the output directory... So create the directory for it...
 mkdir lib/src/generated
+# *Finally* we can actually invoke the compiler and generate the code.
 protoc --dart_out=grpc:lib/src/generated pb/crud.proto
